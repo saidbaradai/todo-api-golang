3d11d91773efbaa300925b6f5cb05fd9ec493cd0
said <saitbaradai@gmail.com>
2023-09-17 05:03:56 +0300

    add : todos GET endpoint

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Read book", Completed: true},
+	{ID: "2", Item: "Clean room", Completed: true},
+	{ID: "3", Item: "Call Sara", Completed: true},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
 
 func main() {
-    fmt.Println("hello world")
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090")
 }
