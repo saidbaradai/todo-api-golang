e3ebc1460bafca9bba0a309f783fc839a98767f4
said <saitbaradai@gmail.com>
2023-09-17 05:33:50 +0300

    feat: add todos POST endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -34,9 +35,30 @@ func addTodo(context *gin.Context) {
 
 }
 
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
+	}
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func getTodoById(id string) (*todo, error) {
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("Todo Not Found!")
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.POST("/todos", addTodo)
+	router.GET("/todos/:id", getTodo)
 	router.Run("localhost:9090")
 }
